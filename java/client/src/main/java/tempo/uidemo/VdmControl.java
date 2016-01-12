f1cc367565c2aad7220ce392fc4ea2fa892ea954
Luis Diogo Couto <ldc@eng.au.dk>
2016-01-12 16:47:21 +0100

    Replace hard coded listener with generic listener attach method.

--- java/client/src/main/java/tempo/uidemo/VdmControl.java
+++ java/client/src/main/java/tempo/uidemo/VdmControl.java
@@ -5,6 +5,8 @@
  */
 package tempo.uidemo;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,12 +14,18 @@ import java.util.logging.Logger;
 import net.java.html.boot.BrowserBuilder;
 
 import org.overture.ast.intf.lex.ILexNameToken;
+import org.overture.ast.types.ACharBasicType;
+import org.overture.ast.types.AIntNumericBasicType;
+import org.overture.ast.types.ASeqSeqType;
+import org.overture.ast.types.PType;
 import org.overture.interpreter.debug.RemoteControl;
 import org.overture.interpreter.debug.RemoteInterpreter;
 import org.overture.interpreter.values.NameValuePairMap;
 import org.overture.interpreter.values.ObjectValue;
 import org.overture.interpreter.values.UpdatableValue;
 import org.overture.interpreter.values.Value;
+
+import com.google.auto.value.AutoValue;
 
 /**
  * Class for controlling model execution via remote interpreter. Also doubles as
@@ -27,10 +35,11 @@ import org.overture.interpreter.values.Value;
  */
 public class VdmControl implements RemoteControl {
 
+	private static final String ROOT_NAME = "root";
 	private static RemoteInterpreter interp;
-	
+
 	public static void callOp() throws Exception {
-		interp.execute("m.CallOp()");
+		interp.execute(ROOT_NAME + ".CallOp()");
 	}
 
 	/**
@@ -60,18 +69,35 @@ public class VdmControl implements RemoteControl {
 	 */
 	public static void onPageLoad() throws Exception {
 		Data d = DataModel.onPageLoad();
-		interp.create("m", "new Control()");
-		Value v = interp.valueExecute("m");
+		
+		ASeqSeqType stringType = new ASeqSeqType();
+		stringType.setSeqof(new ACharBasicType());
+		
+		List<BoundVarInfo> varsToBind = Arrays.asList(new BoundVarInfo[] {BoundVarInfo.create("opCount", new AIntNumericBasicType()),
+				BoundVarInfo.create("opMsg", stringType)});
+		
+		attachListeners(varsToBind, "new Control()", d);
+		
+	}
+
+	public static void attachListeners(List<BoundVarInfo> vars,
+			String rootConstructor, Data d) throws Exception {
+		String root = ROOT_NAME;
+		interp.create(root, rootConstructor);
+		Value v = interp.valueExecute(root);
 		if (v.deref() instanceof ObjectValue) {
 			NameValuePairMap members = ((ObjectValue) v.deref()).members;
-
 			for (Entry<ILexNameToken, Value> p : members.entrySet()) {
-				if (p.getKey().getName().equals("opCount")) {
-					if (p.getValue() instanceof UpdatableValue) {
-						UpdatableValue u = (UpdatableValue) p.getValue();
-						u.addListener(new PropertyListener(d));
+				for (BoundVarInfo bv : vars) {
+					if (bv.name().equals(p.getKey().getName())) {
+						if (p.getValue() instanceof UpdatableValue) {
+							UpdatableValue u = (UpdatableValue) p.getValue();
+							u.addListener(new BoundVarListener(d, bv.name(), bv.type()));
+							break;
+						}
 					}
 				}
+
 			}
 		}
 	}
@@ -79,6 +105,17 @@ public class VdmControl implements RemoteControl {
 	public static void finish() {
 		interp.finish();
 		System.exit(0);
+	}
+
+	@AutoValue
+	abstract static class BoundVarInfo {
+		static BoundVarInfo create(String name, PType type) {
+			return new AutoValue_VdmControl_BoundVarInfo(name, type);
+		}
+
+		abstract String name();
+
+		abstract PType type();
 	}
 
 }
