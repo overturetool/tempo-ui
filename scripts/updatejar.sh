48f306eb61ec92671a595f92e6a4c9930e3db39e
Luis Diogo Couto <ldc@eng.au.dk>
2016-01-13 13:46:01 +0100

    Fix script to build jar without needing installed deps.
    Issue:

--- scripts/updatejar.sh
+++ scripts/updatejar.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 cd $(git rev-parse --show-toplevel) # go to repo root
-mvn -Pdesktop clean package -f java/client/pom.xml &&
+mvn -Pdesktop clean package -f java/pom.xml &&
 unzip -o -d model/lib/ java/client/target/uidemo-1.0-SNAPSHOT-javafx.zip &&
 echo "jar updated"
