799e79312f3f813bd50a3e4243c089f42a9809b5
Luis Diogo Couto <ldc@eng.au.dk>
2016-02-02 17:33:02 +0100

    Add initial UiModelData.

---
+++ example/client/src/main/java/org/overturetool/tempoui/example/UiModelData.java
@@ -0,0 +1,15 @@
+package org.overturetool.tempoui.example;
+
+import net.java.html.json.Model;
+import net.java.html.json.Property;
+
+/**
+ * Model annotation generates class VdmData with
+ * one boolean property named "ok".
+ */
+@Model(className = "VdmData",targetId="", properties = {
+        @Property(name = "ok", type = boolean.class),
+})
+final class UiModelData {
+
+}
