4bfa7b5c183ee640349ffb86a9c4c2158a71b2c3
Luis Diogo Couto <ldc@eng.au.dk>
2016-02-02 18:54:59 +0100

    Update example to use new var initialising features.

--- example/client/src/main/java/org/overturetool/tempoui/example/ExampleRC.java
+++ example/client/src/main/java/org/overturetool/tempoui/example/ExampleRC.java
@@ -1,9 +1,9 @@
 package org.overturetool.tempoui.example;
 
 import org.overture.ast.types.ABooleanBasicType;
-import org.overture.interpreter.debug.RemoteControl;
+import org.overture.interpreter.runtime.Interpreter;
 import org.overture.interpreter.values.Value;
-import org.overturetool.tempoui.ListenerAttacher;
+import org.overturetool.tempoui.ModelBinder;
 import org.overturetool.tempoui.TempoRemoteControl;
 
 import java.util.ArrayList;
@@ -18,7 +18,6 @@ public class ExampleRC extends TempoRemoteControl {
         // Create and bind DukeScript Data Model
         VdmData uiData = new VdmData();
         uiData.applyBindings();
-        uiData.setOk(true);
 
         // Build and execute entry point
         String root = "plant";
@@ -26,11 +25,12 @@ public class ExampleRC extends TempoRemoteControl {
         interpreter.create(root, rootConstructor);
         Value v = interpreter.valueExecute(root);
 
+
         // Attach listeners to UI properties to VDM variables
-        List<ListenerAttacher.VarBindInfo> vars = new ArrayList<>(1);
-        vars.add(ListenerAttacher.VarBindInfo.create("ok", new ABooleanBasicType()));
-        ListenerAttacher la = new ListenerAttacher(v, vars, uiData);
-        la.attach();
+        List<ModelBinder.VarBindInfo> vars = new ArrayList<>(1);
+        vars.add(ModelBinder.VarBindInfo.create("ok", new ABooleanBasicType()));
+        ModelBinder la = new ModelBinder(v, vars, uiData);
+        la.bind();
 
     }
 
