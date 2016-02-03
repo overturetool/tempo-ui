7fcee955c87db2e1ae754eadea6835df3ab8d3a7
Luis Diogo Couto <ldc@eng.au.dk>
2016-02-03 17:21:09 +0100

    Expose the new ModelAPI methods through data model

--- example/client/src/main/java/org/overturetool/tempoui/example/UiDataModel.java
+++ example/client/src/main/java/org/overturetool/tempoui/example/UiDataModel.java
@@ -1,15 +1,33 @@
 package org.overturetool.tempoui.example;
 
+import net.java.html.json.Function;
 import net.java.html.json.Model;
 import net.java.html.json.Property;
+
+import java.util.List;
 
 /**
  * Model annotation generates class VdmData with
  * one boolean property named "ok".
  */
-@Model(className = "VdmData",targetId="", properties = {
-        @Property(name = "ok", type = boolean.class),
+@Model(className = "VdmData", targetId = "", properties = {
+        @Property(name = "statusOk", type = boolean.class)
 })
 final class UiDataModel {
+
+    @Function
+    public static void addAlarm(VdmData d, Alarm a, String desc) throws Exception {
+        ModelAPI.addAlarm(a, desc);
+    }
+
+    public enum Alarm {
+        Elec, Mech, Bio, Chem
+
+    }
+
+    @Function
+    static void addExpert(String id, Alarm quali, String period) throws Exception {
+        ModelAPI.addExpert(id,quali, period);
+    }
 
 }
