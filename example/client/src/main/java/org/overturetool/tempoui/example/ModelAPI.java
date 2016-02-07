8c8337f9b1c4b9b6a1ea84ac8c79d189ab547bab
Luis Diogo Couto <ldc@eng.au.dk>
2016-02-07 01:49:43 +0100

    Add support for adding and listing experts.

--- example/client/src/main/java/org/overturetool/tempoui/example/ModelAPI.java
+++ example/client/src/main/java/org/overturetool/tempoui/example/ModelAPI.java
@@ -1,7 +1,9 @@
 package org.overturetool.tempoui.example;
 
 import org.overture.interpreter.debug.RemoteInterpreter;
+import org.overture.interpreter.values.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -10,33 +12,54 @@ import java.util.List;
 public class ModelAPI {
 
     public static final String ROOT_NAME = "plant";
+    private static int expertCounter = 0;
+
+    public static void setInterp(RemoteInterpreter interp) {
+        ModelAPI.interp = interp;
+    }
 
     private static RemoteInterpreter interp;
 
-    public static void addAlarm(UiDataModel.Alarm kind, String desc) throws Exception {
-        // VDM: AddAlarm(mk_Alarm(desc,<kind>))
-        StringBuilder cmd =  new StringBuilder();
-        cmd.append("AddAlarm(mk_Alarm(");
-        cmd.append(desc);
-        cmd.append("),<");
-        cmd.append(kind);
-        cmd.append(">))");
-        interp.execute(cmd.toString());
+    public static List<Expert> getAllExperts() throws Exception {
+        StringBuilder cmd = new StringBuilder();
+        cmd.append("schedule");
+        ValueMap sched = interp.valueExecute(cmd.toString()).deref().mapValue(null);
+
+        List<Expert> r = new ArrayList<>();
+
+        for (Value k : sched.keySet()) {
+            for (Value e : sched.get(k).deref().setValue(null)) {
+
+                RecordValue rec = (RecordValue) e.deref();
+                r.add(new Expert(extractId(rec.fieldmap.get("expertid")), extractId(k),
+                        UiDataModel.string2Enum(rec.fieldmap.get("quali").quoteValue(null))));
+            }
+        }
+        return r;
     }
 
-    public static void addExpert(String id, UiDataModel.Alarm quali, String period) throws Exception {
+    private static String extractId(Value s) {
+        return s.deref().toString().trim().replace("mk_token(\"", "").replace("\")", "");
+    }
+
+
+    public static void addExpert(UiDataModel.Alarm quali, String period) throws Exception {
         //AssignExpert(mk_Expert(mk_token("e1"),<Chem>),mk_token("p1))
-        StringBuilder cmd =  new StringBuilder();
-        cmd.append("AssignExpert(mk_Expert(mk_token(");
-        cmd.append(id);
-        cmd.append("),<");
+        StringBuilder cmd = new StringBuilder();
+        cmd.append("AssignExpert(mk_Expert(mk_token(\"");
+        cmd.append(makeExpertId());
+        cmd.append("\"),");
         cmd.append(quali);
-        cmd.append(">),mk_token(");
+        cmd.append("),mk_token(\"");
         cmd.append(period);
-        cmd.append("))");
+        cmd.append("\"))");
         interp.execute(cmd.toString());
     }
 
+    private static String makeExpertId() {
+        expertCounter++;
+        return "E" + Integer.toString(expertCounter);
+    }
 
 
 }
--- example/client/src/main/java/org/overturetool/tempoui/example/UiDataModel.java
+++ example/client/src/main/java/org/overturetool/tempoui/example/UiDataModel.java
@@ -1,9 +1,11 @@
 package org.overturetool.tempoui.example;
 
+import net.java.html.json.ComputedProperty;
 import net.java.html.json.Function;
 import net.java.html.json.Model;
 import net.java.html.json.Property;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,23 +13,53 @@ import java.util.List;
  * one boolean property named "ok".
  */
 @Model(className = "VdmData", targetId = "", properties = {
-        @Property(name = "statusOk", type = boolean.class)
+        @Property(name = "statusOk", type = boolean.class),
+        @Property(name = "expertQuali", type = String.class),
+        @Property(name = "expertId", type = String.class),
+        @Property(name = "expertP", type = String.class),
+        @Property(name = "updateFlag", type = boolean.class),
+        @Property(name = "periodIds", type = String.class, array = true),
 })
 final class UiDataModel {
 
-    @Function
-    public static void addAlarm(VdmData d, Alarm a, String desc) throws Exception {
-        ModelAPI.addAlarm(a, desc);
+
+    @Model(className = "Expert", properties = {
+            @Property(name = "id", type = String.class),
+            @Property(name = "period", type = String.class),
+            @Property(name = "quali", type = Alarm.class),
+    })
+    public static class ExpertModel {
     }
 
     public enum Alarm {
-        Elec, Mech, Bio, Chem
+        Elec, Mech, Bio, Chem;
 
+        @Override
+        public String toString() {
+            return "<" + super.toString() + ">";
+        }
+    }
+
+    @ComputedProperty
+    static java.util.List<Expert> experts(boolean updateFlag) {
+        List<Expert> r = new ArrayList<>();
+        try {
+            r.addAll(ModelAPI.getAllExperts());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return r;
     }
 
     @Function
-    static void addExpert(String id, Alarm quali, String period) throws Exception {
-        ModelAPI.addExpert(id,quali, period);
+    static void addExpert(VdmData model, String data) throws Exception {
+        ModelAPI.addExpert(string2Enum(model.getExpertQuali()), model.getExpertP());
+        model.setUpdateFlag(!model.isUpdateFlag());
+    }
+
+    public static Alarm string2Enum(String s) {
+        String s1 = s.replaceAll("<|>", "");
+        return Alarm.valueOf(s1);
     }
 
 }
